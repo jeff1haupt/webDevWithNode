bf80e35b3edb7994fd66c004ab14b24a3f3f2830
Jeff Haupt <jhaupt@southbendin.gov>
2023-03-10 09:17:19 -0500

    Added static webpage example

--- Chapter2/helloworld.js
+++ Chapter2/helloworld.js
@@ -1,5 +1,17 @@
 const http = require('http');
-const port = process.env.PORT || 3000
+const fs = require('fs');
+const port = process.env.PORT || 3000;
+
+function serveStaticFile( res, path, contentType, responseCode = 200) {
+    fs.readFile(__dirname + path, (err, data) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            return res.end('500 - Internal Error');
+        }
+        res.writeHead(responseCode, { 'Content-Type': contentType });
+        res.end(data);
+    });
+}
 
 const server = http.createServer((req, res) => {
     // normalize url be removing querystring, optional
@@ -7,16 +19,16 @@ const server = http.createServer((req, res) => {
     const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
     switch (path) {
         case '':
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end('Homepage');
+            serveStaticFile(res, '/public/home.html', 'text/html')
             break;
         case '/about':
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end('About');
+            serveStaticFile(res, '/public/about.html', 'text/html')
+            break;
+        case '/img/logo.png':
+            serveStaticFile(res, '/public/img/logo.png', 'image/png')
             break;
         default:
-            res.writeHead(404, { 'Content-Type': 'text/plain' });
-            res.end('Not Found');
+            serveStaticFile(res, '/public/404.html', 'text/html')
             break;
     }
 });
