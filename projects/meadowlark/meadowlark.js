e7f785bc71319173b0647300c54993b33d4b940c
Jeff Haupt <jhaupt@southbendin.gov>
2023-03-12 16:47:55 -0400

    Moved fortune cooke into module

--- projects/meadowlark/meadowlark.js
+++ projects/meadowlark/meadowlark.js
@@ -1,15 +1,8 @@
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
+const fortune = require('./lib/fortune');
 
 const app = express();
-
-const fortunes = [
-    'Conquer your fears or they will conquer you.',
-    'Rivers need springs.',
-    'Do not fear what you dont know.',
-    'You will have a pleasant surprise.',
-    'Whenever possible, keep it simple.'
-];
 
 app.engine('handlebars', expressHandlebars.engine({
     defaultLayout: 'main',
@@ -22,8 +15,7 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => res.render('home'));
 
 app.get('/about', (req, res) => {
-    const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)];
-    res.render('about', { fortune: randomFortune });
+    res.render('about', { fortune: fortune.getFortune() });
 });
 
 // custom 404 page
