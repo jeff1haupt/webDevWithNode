fc58dea6948009e6ee4cd8af695984f79390ae60
Jeff Haupt <jhaupt@southbendin.gov>
2023-03-12 17:13:54 -0400

    Ch5 - created handles module

--- projects/meadowlark/meadowlark.js
+++ projects/meadowlark/meadowlark.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
-const fortune = require('./lib/fortune');
-
+const handlers = require('./lib/handlers');
 const app = express();
 
 app.engine('handlebars', expressHandlebars.engine({
@@ -12,24 +11,12 @@ app.set('view engine', 'handlebars');
 const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => res.render('home'));
+app.get('/', handlers.home);
+app.get('/about', handlers.about);
 
-app.get('/about', (req, res) => {
-    res.render('about', { fortune: fortune.getFortune() });
-});
-
-// custom 404 page
-app.use( (req, res) => {
-    res.status(404);
-    res.render('404');
-});
-
-// custom 500 page
-app.use( (err, req, res, next) => {
-    console.error(err.message);
-    res.status(500);
-    res.render('500');
-});
+// custom 404 and 500 pages
+app.use(handlers.notFound);
+app.use(handlers.serverError);
 
 app.listen( port, () => console.log(
     `Express started on http://localhost:${port}: ` +
